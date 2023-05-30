ca9399c8c5e6bc39a675850995de78dc0fc4be77
VIPigeon <slv.bta@gmail.com>
2023-05-30 12:48:19 +0300

    Базированный картридж

---
+++ Cart/Game.lua
@@ -0,0 +1,19 @@
+Game = {}
+
+
+function Game:new()
+    obj = {
+        mode = 'debug'
+    }
+    -- чистая магия!
+    setmetatable(obj, self)
+    self.__index = self; return obj
+end
+
+
+function Game:update()
+    rect(1, 1, 10, 10, 2)
+end
+
+
+return Game
