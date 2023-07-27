1c987ce35ed022b0e85408fd27b0ec74769d197e
Linux Torbolts <114951816+MakimaGorky@users.noreply.github.com>
2023-07-27 11:09:43 +0300

    BulletHeh

--- Cart/BulletHell.lua
+++ Cart/BulletHell.lua
@@ -7,13 +7,32 @@ function BulletHell:new(x, y, circleDiameter, bulletSpreadRadius, bullets)
         spread = bulletSpreadRadius,
         bullets = bullets,
         bulletCount = #bullets,
+        bulletSpeed = 0.5,
+        bulletRotateSpeed = 1,
         hitCircle = HitCircle:new(x, y, circleDiameter),
     }
 
     setmetatable(object, self)
 end
 
+function BulletHell:shoot()
+    local minDist = 2147483647
+    local minId = -1
+    for i, bull in ipairs(self.bullets) do
+        local dist = math.sqrt(math.sqr(game.player.x - bull.x) + math.sqr(game.player.y - bull.y))
+        if dist < minDist then
+            minDist = dist
+            minId = i
+        end
+    end
+
+    bullets[minId - 2]:shoot()
+end
+
 function BulletHell:update()
+    if game.metronome.on_beat then
+        self:shoot()
+    end
 end
 
 function BulletHell:draw()
